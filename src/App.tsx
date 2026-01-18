c1605ba1265c8d680912559e536d57d2f1c36608
daredevilUK3000 <121494710+daredevilUK3000@users.noreply.github.com>
2026-01-18 11:16:11 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import Index from "./pages/Index";
 import Compare from "./pages/Compare";
 import Quiz from "./pages/Quiz";
 import Reviews from "./pages/Reviews";
+import Contact from "./pages/Contact";
+import Help from "./pages/Help";
+import Privacy from "./pages/Privacy";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/compare" element={<Compare />} />
-      <Route path="/quiz" element={<Quiz />} />
-      <Route path="/reviews" element={<Reviews />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </HashRouter>
+<HashRouter>
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route path="/compare" element={<Compare />} />
+    <Route path="/quiz" element={<Quiz />} />
+    <Route path="/reviews" element={<Reviews />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/help" element={<Help />} />
+    <Route path="/privacy" element={<Privacy />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</HashRouter>
 );
 
 export default App;
