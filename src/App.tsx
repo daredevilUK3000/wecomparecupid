c2a244c2632eac89c2cebe753d1329323ee74bf6
daredevilUK3000 <121494710+daredevilUK3000@users.noreply.github.com>
2026-01-19 07:18:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,3 +24,39 @@ const App = () => (
 );
 
 export default App;
+
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { initGA } from "./lib/analytics";
+import Index from "./pages/Index";
+import Compare from "./pages/Compare";
+import Quiz from "./pages/Quiz";
+import Reviews from "./pages/Reviews";
+import Contact from "./pages/Contact";
+import Help from "./pages/Help";
+import Privacy from "./pages/Privacy";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  useEffect(() => {
+    // Initialize Google Analytics
+    initGA();
+  }, []);
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default App;
